c008812dfadbbac2c989945d1024ec56d55e6d95
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-12 12:07:37 +0100

    L19: Deserialization with Marshmallow

--- MarshmallowIntro/serialization.py
+++ MarshmallowIntro/serialization.py
@@ -2,10 +2,12 @@ from marshmallow import Schema, fields
 
 
 class BookSchema(Schema):
-    title = fields.Str()
+    title = fields.Str()  # can be required=True - won't work without 'title' key. Defalt = false
     author = fields.Str()
+    description = fields.Str()
 
 
+# Serialization
 class Book:
     def __init__(self, title, author, description):
         self.title = title
@@ -13,9 +15,23 @@ class Book:
         self.description = description
 
 
-book = Book("Clean Code", "Bob Martin", "A book about writing cleaner code.")
+book_ser = Book("Clean Code", "Bob Martin", "A book about writing cleaner code.")
 
 book_schema = BookSchema()
-book_dict = book_schema.dump(book)
+book_dict = book_schema.dump(book_ser)
 
 print(book_dict)
+
+# Deserialization
+
+incoming_book_data = {
+    "title": "Clean Code",
+    "author": "Bob Martin",
+    "description": "A book about writing cleaner code.",
+}
+
+book_de = book_schema.load(incoming_book_data)
+book_obj = Book(**book_de)
+
+print(book_de)
+print(book_obj)
