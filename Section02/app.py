590eca6a28a4b43c1c741aabb76056ecbaa93223
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-03-14 13:47:27 +0100

    l24: Adding Items to Api

--- Section02/app.py
+++ Section02/app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
+from resources.item import Item, ItemList
 from blacklist import BLACKLIST
 
 from ma import ma
@@ -38,6 +39,8 @@ def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token["jti"] in BLACKLIST
 
 
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
